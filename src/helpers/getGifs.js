1fb7a400503a084b00739e01ea73e56c742053d0
Angel Lopez <angellopez15luisa@gmail.com>
2022-02-18 04:00:35 -0500

    First commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,23 @@
+
+ const getGifs = async (category) => {
+
+        const url = `https://api.giphy.com/v1/gifs/search?api_key=MPbiOjRcmjouYPAMsp22QSkJvlLpb7dP&q=${ encodeURI(category)}&limit=10`;
+        const resp = await fetch(url);
+        const { data } = await resp.json();
+
+        const gifs = data.map(img => {
+            return {
+                id: img.id,
+                title: img.title,
+                url: img.images?.downsized_medium.url
+            }
+        })
+        
+     return gifs;
+     //   setImages(gifs);
+}
+    
+
+export default getGifs;
+
+    
